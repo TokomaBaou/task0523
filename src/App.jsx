bb76567bbc68b04efe576cc18832fd38a02c1912
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-23 22:49:35 +0900

    incomp

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,23 @@ export const App = () => {
   //未完了に追加する配列を作る
   const [incompTodo, setIncompTodo] = useState([]);
 
+  //変更したときstateを変更する関数
   const onChangeTodo = (e) => setTodoText(e.target.value);
+  //追加ボタンを押したとき未完了の配列にセットする関数
+  const onChlickAdd = () => {
+    if (todoText === "") return;
+    const setNewTodo = [...incompTodo, todoText];
+    setIncompTodo(setNewTodo);
+    setTodoText("");
+  };
+
   return (
     <div className="App">
-      <Input todoText={todoText} onChange={onChangeTodo} />
+      <Input
+        todoText={todoText}
+        onChange={onChangeTodo}
+        onClick={onChlickAdd}
+      />
     </div>
   );
 };
